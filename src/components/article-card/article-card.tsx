fa58737cddc9e9fe3a75be570ccdd60c8912ba49
Konstantinos Koliopoulos <konst.koliop@gmail.com>
2025-04-01 00:01:12 +0300

    Make routes work in dev & add transitions to index page links

--- src/components/article-card/article-card.tsx
+++ src/components/article-card/article-card.tsx
@@ -6,7 +6,7 @@ export default component$<{
 }>((props) => {
   return (
     <a href={props.href}>
-      <article class="flex flex-col items-center gap-1 border-2 border-black rounded-sm p-2">
+      <article class="flex flex-col items-center gap-1 rounded-sm border-2 border-black p-2 transition duration-200 ease-in-out not-motion-reduce:hover:-translate-y-1 not-motion-reduce:hover:scale-110 not-motion-reduce:focus:-translate-y-1 not-motion-reduce:focus:scale-110 motion-reduce:transition-none">
         <Slot />
         <h2 class="text-2xl">{props.title}</h2>
       </article>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,13 +7,27 @@ import NormalGraph from "~/components/images/normal-graph";
 export default component$(() => {
   return (
     <>
-      <h1 class="mt-1.5 text-center text-4xl">Κατανομές</h1>
+      <h1 class="mt-2 text-center text-4xl">Κατανομές</h1>
       <div class="mt-5 flex justify-center">
-        <main class="grid grid-cols-2 gap-5">
-          <ArticleCard title="Διωνυμική" href="/quick-dirty-distributions/binomial">
+        <main class="mb-2 grid grid-cols-2 gap-5">
+          <ArticleCard
+            title="Διωνυμική"
+            href={
+              import.meta.env.DEV
+                ? "/binomial"
+                : "/quick-dirty-distributions/binomial"
+            }
+          >
             <BinomialGraph />
           </ArticleCard>
-          <ArticleCard title="Κανονική" href="/quick-dirty-distributions/normal">
+          <ArticleCard
+            title="Κανονική"
+            href={
+              import.meta.env.DEV
+                ? "/normal"
+                : "/quick-dirty-distributions/normal"
+            }
+          >
             <NormalGraph />
           </ArticleCard>
         </main>
